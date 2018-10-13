e32e1488af38d26a9477765dc81afe9497fdc9d2
Keith Pounds <keith.pounds@autozone.com>
2018-10-13 09:27:05 -0500

    Adding chartjs component

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Bar } from 'react-chartjs-2';
 import axios from 'axios';
 import './app.css';
+import { redBright } from 'ansi-colors';
 
 export default class App extends Component {
   state = { averageWith: null, averageWithout: null };
@@ -26,9 +28,41 @@ export default class App extends Component {
 
   render() {
     const { averageWith, averageWithout } = this.state;
+    const barData = {
+      labels: ['Without Internet', 'With Internet'],
+      datasets: [
+        {
+          label: 'Average grade based on internet access',
+          data: [parseFloat(averageWithout), parseFloat(averageWith)],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 206, 86, 0.2)'
+          ],
+          borderColor: ['rgba(255,99,132,1)', 'rgba(255, 206, 86, 1)'],
+          borderWidth: 1
+        }
+      ]
+    };
     return (
       <div>
         <h1>Hello Student!</h1>
+        <Bar
+          data={barData}
+          width={100}
+          height={20}
+          options={{
+            maintainAspectRatio: true,
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }
+              ]
+            }
+          }}
+        />
         <div>{`Average without internet: ${averageWithout}`}</div>
         <div>{`Average with internet: ${averageWith}`}</div>
       </div>
